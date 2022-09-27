d0bf26db58833bed5c733f37cc3bb3d3850c02dd
Harshita_Mathpal <91454889+HarshitaMathpal@users.noreply.github.com>
2022-09-27 20:20:01 +0530

    Create Greatest English Letter in Upper and Lower Case.java

---
+++ Greatest English Letter in Upper and Lower Case.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String greatestLetter(String s) {
+        char ch = 'z';
+        char CH = 'Z';
+        for(int i=0;i<26;i++){
+            if(s.indexOf(ch) != -1 && s.indexOf(CH) != -1){
+                return CH + "";
+            } 
+            else{
+                ch--;
+                CH--;
+            }
+        }
+        return "";
+    }
+}
