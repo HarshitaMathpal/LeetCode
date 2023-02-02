85362534b4f0a89bd3fe520de631dc8be2c07b0a
Harshita_Mathpal <91454889+HarshitaMathpal@users.noreply.github.com>
2023-02-02 14:42:07 +0530

    Create Binary Tree Cameras.java

---
+++ Binary Tree Cameras.java
@@ -0,0 +1,30 @@
+class Solution {
+    int count=0;
+    public int minCameraCover(TreeNode root) {
+        int c=CountCamera(root);
+        if(c==-1){
+            count++;
+        }
+        return count;
+    }
+
+    public int CountCamera(TreeNode root){
+        if(root==null){
+            return 1;
+        }
+        int left=CountCamera(root.left);
+        int right=CountCamera(root.right);
+
+        if(left==-1 || right==-1){
+            count++;
+            return 2;
+        }
+        if(left==1 && right==1){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+
+    }
+}
