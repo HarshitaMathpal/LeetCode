95b09160a1776b9a58601638546a480346598316
Harshita_Mathpal <91454889+HarshitaMathpal@users.noreply.github.com>
2023-03-15 13:36:59 +0530

    Create Rearrange Array to Maximize Prefix Score.java

---
+++ Rearrange Array to Maximize Prefix Score.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxScore(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        // int maxsum = 0;
+        long sum = 0;
+        for(int i=nums.length-1;i>=0;i--){
+            sum += nums[i];
+            if(sum>0){
+                ans++;
+            }
+            else
+                break;
+        }
+
+        return ans;
+
+    }
+}
